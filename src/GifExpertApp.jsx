9220ba4512c093c39833207b798ea70c0e8cc365
Daniel M <daniel.munoz@interax.com.mx>
2025-06-03 16:39:23 -0600

    Primer Commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import { AddCategory, GifGrid } from "./Components";
+
+export const GifExpertApp = () => {
+
+
+const [categories, setCategories] = useState(['One Punch']);
+
+const onAddCategory = ( newCategory ) => {
+    if (categories.includes(newCategory)) return;
+    //const newCategory = 'Naruto';
+    //categories.push(newCategory);
+    //console.log(newCategory);
+    setCategories([newCategory, ...categories]);
+    //setCategories( cat => [...cat, 'Valorant']);
+}
+
+console.log(categories);
+
+
+    return (
+        <>
+            <h1>GifExpertApp</h1>
+
+            <AddCategory
+                
+                onNewCategory={ (value) => onAddCategory(value) }
+             />
+
+          
+            { categories.map( category => (
+                (
+                    <GifGrid key={ category } category={ category }/>
+                )
+            ) ) }
+        </>
+
+        
+    )
+}
